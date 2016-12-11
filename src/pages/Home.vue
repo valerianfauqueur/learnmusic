4b18d60e85135821f1c2533f0f3668331ce04c1b
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-12-11 13:42:55 +0100

    [global:routing] Fix link target

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home">
     <router-link to="/vibration#air">Vibration</router-link>
-    <router-link to="/vibration#air">Frequency</router-link>
+    <router-link to="/frequency">Frequency</router-link>
   </div>
 </template>
 
