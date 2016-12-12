8bd09a9514310f1bd49ae7a32192bb3fd59c476b
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-12-12 14:30:59 +0100

    [pages:home] Add frequency page

---
+++ src/pages/Frequency.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="frequency">
+    <sound src='../assets/audio/bell/bell.mp3'></sound>
+    <sound src='../assets/audio/bell/bell.mp3'></sound>
+    <sound src='../assets/audio/bell/bell.mp3'></sound>
+    <sound src='../assets/audio/bell/bell.mp3'></sound>
+  </div>
+</template>
+
+<script>
+import Sound from '../components/Sound';
+
+export default {
+  name: 'frequency',
+  components: {
+    Sound,
+  },
+};
+</script>
+
+<style>
+
+</style>
