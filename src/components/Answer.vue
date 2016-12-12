66ae8e0990258e91b92be18d0d645c94f8af0fb8
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-12-12 14:30:27 +0100

    [component:modal] Add answer component

---
+++ src/components/Answer.vue
@@ -0,0 +1,57 @@
+<template>
+    <div @click.stop="validate" class="question__answer" v-bind:class="classObject">{{ answer }}</div>
+</template>
+
+<script>
+
+export default {
+  name: 'answer',
+  props: ['isTrue', 'answer'],
+  data() {
+    return {
+      showAnswer: false,
+    };
+  },
+  methods: {
+    validate() {
+      this.showAnswer = true;
+    },
+  },
+  computed: {
+    classObject() {
+      return {
+        'question__answer--true': this.isTrue && this.showAnswer,
+        'question__answer--false': !this.isTrue && this.showAnswer,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.question__answer {
+  border-radius: 50%;
+  border: 2px solid black;
+  height: 150px;
+  width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 20px;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 24px;
+  text-align: center;
+  transition: all 0.3s ease;
+  cursor: pointer;
+}
+
+.question__answer--false {
+  color:white;
+  background-color: crimson;
+}
+
+.question__answer--true {
+  color:white;
+  background-color: green;
+}
+</style>
