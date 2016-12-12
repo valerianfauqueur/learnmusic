4897c512b4dad1c7f0ced01560e1318c207a09e3
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-12-12 14:30:38 +0100

    [component:modal] Add sound component

---
+++ src/components/Sound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-on:click="playSound" class="sound"></div>
+</template>
+
+<script>
+import Tone from 'tone';
+
+export default {
+  name: 'bell',
+  props: ['frequency', 'src'],
+  methods: {
+    playSound() {
+      const bellSampler = new Tone.Sampler(
+        this.src,
+        () => {
+          bellSampler.triggerAttack(0);
+        }
+      ).toMaster();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sound {
+  width:200px;
+  height:200px;
+  background-color: blue;
+  display: inline-block;
+  margin-left: 20px;
+}
+</style>
