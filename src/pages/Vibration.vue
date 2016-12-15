875166a3a9ca0daa3ce15b976999ea9273e6b92b
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-12-15 10:39:22 +0100

    [pages:vibration] Update scss and html

--- src/pages/Vibration.vue
+++ src/pages/Vibration.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="vibration">
     <div class='orchestra'></div>
-    <div id='space' class='vibration__space'></div>
+    <ul class='vibration__nav'>
+      <li @click="scrollButton('space')" class="vibration__navItem vibration__navItem--space"></li>
+      <li @click="scrollButton('air')" class="vibration__navItem vibration__navItem--orchestra"></li>
+      <li @click="scrollButton('water')" class="vibration__navItem vibration__navItem--water"></li>
+    </ul>
+    <div id='space' class='vibration__space'>
+      <div class='earth'></div>
+      <div class='decoration satellite'></div>
+    </div>
     <div id='air' class='vibration__air'>
       <div class='scroll scroll--top'></div>
       <div class='clouds'>
@@ -10,13 +18,14 @@
       <div class='scroll scroll--bottom'></div>
     </div>
     <div id='water' class='vibration__water'>
-        <div class='waves'>
-          <div class="wave wave_1"></div>
-          <div class="wave wave_2"></div>
-          <div class="wave wave_3"></div>
-          <div class="wave wave_4"></div>
-          <div class="wave wave_5"></div>
-        </div>
+      <div class='decoration algue--2'></div>
+      <div class='decoration algue--1'></div>
+      <div class='decoration algue--3'></div>
+      <div class='decoration wave--1'></div>
+      <div class='decoration wave--2'></div>
+      <div class='decoration wave--3'></div>
+      <div class='banc__poisson--1'></div>
+      <div class='banc__poisson--2'></div>
     </div>
   </div>
 </template>
@@ -96,6 +105,23 @@ export default {
       setTimeout(() => { this.currentlyScrolling = false; }, timeOfScroll + 10);
     },
 
+    scrollButton(env) {
+      this.currentScrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+      if (this.currentlyScrolling === false) {
+        this.currentlyScrolling = true;
+        if (this.currentEnv === 'air') {
+          if (env === 'water') { this.scrollTo(this.currentScrollPos + this.oneScreenHeight, 'down'); }
+          if (env === 'space') { this.scrollTo(this.currentScrollPos - this.oneScreenHeight, 'up'); }
+        } else if (this.currentEnv === 'space') {
+          if (env === 'air') { this.scrollTo(this.currentScrollPos + this.oneScreenHeight, 'down'); }
+          if (env === 'water') { this.scrollTo(this.currentScrollPos + (this.oneScreenHeight * 2), 'down'); }
+        } else if (this.currentEnv === 'water') {
+          if (env === 'air') { this.scrollTo(this.currentScrollPos - this.oneScreenHeight, 'up'); }
+          if (env === 'space') { this.scrollTo(this.currentScrollPos - (this.oneScreenHeight * 2), 'up'); }
+        }
+      }
+    },
+
     oneScreen() {
       this.oneScreenHeight = window.innerHeight ||
         document.documentElement.clientHeight ||
@@ -117,7 +143,7 @@ export default {
       } else if (this.currentScrollPos < this.minScrollPos + this.oneScreenHeight) {
         this.currentEnv = 'space';
         if (this.lastEnv !== this.currentEnv) {
-          this.currentSound.stop();
+          this.currentSound.pause();
         }
       } else if (this.currentScrollPos > this.maxScrollPos - this.oneScreenHeight) {
         this.currentEnv = 'water';
@@ -150,18 +176,41 @@ export default {
 
 <style lang="scss" scoped>
 
+.decoration {
+  position: absolute;
+  background-size: cover;
+}
+
 .vibration__space {
-  height:105vh;
+  height:100vh;
   width:100%;
-  background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="246" height="2000" viewBox="0 0 246 2000"><path fill-rule="evenodd" clip-rule="evenodd" fill="#000" d="M0 2000c50.43 0 72.57-20.522 123-20.522 50.43 0 71.34 20.522 123 20.522V0H0v2000z"/></svg>');
-  background-position:bottom left;
-  background-repeat-y: no-repeat;
+  background-color: black;
+  position: relative;
+}
+
+.satellite {
+  top: 15vh;
+  left: 20vw;
+  width: 101px;
+  height: 84px;
+  background-image: url('../assets/img/vibration/satellite-station.png');
+}
+
+.earth {
+  position: absolute;
+  bottom: 0;
+  right:0;
+  background-image: url('../assets/img/vibration/earth.png');
+  width: 40vw;
+  height: 29vw;
+  background-size: cover;
 }
 
 .vibration__air {
-  height:90vh;
+  height:100vh;
   width:100%;
   position: relative;
+  background-color: #3e3e40;
 }
 
 .scroll {
@@ -192,8 +241,8 @@ export default {
 .clouds__animation {
   position: relative;
   height: 100%;
-  background-image: url('../assets/img/clouds1000.png'),
-                    url('../assets/img/clouds1000.png');
+  background-image: url('../assets/img/vibration/clouds1000.png'),
+                    url('../assets/img/vibration/clouds1000.png');
   background-repeat: repeat-x;
 	animation: wind 30s linear infinite;
   transform: translateZ(0deg);
@@ -209,6 +258,38 @@ export default {
   }
 }
 
+.vibration__nav {
+  position: fixed;
+  left:0;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index:3;
+}
+
+.vibration__navItem {
+  width: 78px;
+  height: 78px;
+  margin: 2vh 3vw;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  cursor: pointer;
+}
+
+.vibration__navItem--space {
+  background-image: url('../assets/img/vibration/button-space.png');
+}
+
+.vibration__navItem--orchestra {
+  background-image: url('../assets/img/vibration/button-trumpet.png');
+}
+
+.vibration__navItem--water {
+  background-image: url('../assets/img/vibration/button-sea.png');
+}
 
 .orchestra  {
   background-color: yellow;
@@ -217,119 +298,76 @@ export default {
   position:fixed;
   top: calc(50vh - 100px);
   left: calc(50% - 100px);
+  z-index: 2;
 }
 
-.waves
-{
-	width:100%;
-	height:105vh;
-  min-height:10em;
-  overflow: hidden;
+.vibration__water {
+  height:100vh;
   position: relative;
+  background: linear-gradient(45deg, #105376 0%,#4da2ce 100%);
 }
 
-.wave
-{
-	width:calc( 100% + 4em );
-	height:100%;
-	position:absolute;
-	left:-2em;
-	background:bottom center repeat-x;
-	animation-iteration-count:infinite;
-	animation-timing-function:linear;
+.algue--1 {
+  bottom:0;
+  left: 20vw;
+  width: 198px;
+  height: 316px;
+  background-image: url('../assets/img/vibration/seaweed.png');
 }
 
-/* Individual wave layers */
-
-.wave_1
-{
-  animation-name:wave_1;
-  animation-duration:3400ms;
-  animation-delay:-1200ms;
-  top:1.0em;
-  z-index:1;
-  opacity:0.1;
-  background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="246" height="2000" viewBox="0 0 246 2000"><path transform="rotate(-180 123,999.5)" fill-rule="evenodd" clip-rule="evenodd" fill="#153C65" d="M0 2000c50.43 0 72.57-20.522 123-20.522 50.43 0 71.34 20.522 123 20.522V0H0v2000z"/></svg>');
-  background-position:top left;
+.algue--2 {
+  bottom:0;
+  left: 27.5vw;
+  width: 162px;
+  height: 178px;
+  background-image: url('../assets/img/vibration/seaweed2.png');
 }
 
-.wave_2
-{
-  animation-name:wave_2;
-  animation-duration:3200ms;
-  animation-delay:-600ms;
-  top:2.0em;
-  z-index:2;
-  opacity:0.2;
-  background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="307" height="2000" viewBox="0 0 185 2000"><path transform="rotate(-180 123,999.5)" fill-rule="evenodd" clip-rule="evenodd" fill="#153C65" d="M0 0h307v2000c-64.47 0-90.563-25.623-153.5-25.623C90.565 1974.377 62.935 2000 0 2000V0z"/></svg>');
-  background-position:top right;
+.algue--3 {
+  bottom:0;
+  left: 80vw;
+  width: 218px;
+  height: 246px;
+  background-image: url('../assets/img/vibration/seaweed1.png');
 }
 
-.wave_3
-{
-  animation-name:wave_3;
-  animation-duration:2800ms;
-  animation-delay:-2400ms;
-  top:3.0em;
-  z-index:3;
-  opacity:0.3;
-  background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="384" height="2000" viewBox="0 0 108 2000"><path transform="rotate(-180 123,999.5)" fill-rule="evenodd" clip-rule="evenodd" fill="#153C65" d="M0 0h384v2000c-80.64 0-113.28-32.047-192-32.047S78.72 2000 0 2000V0z"/></svg>');
-  background-position:top center;
+.banc__poisson--1 {
+  position: absolute;
+  bottom: 30vh;
+  right: 10vw;
+  width: 20vw;
+  height: 20vh;
 }
 
-.wave_4
-{
-  animation-name:wave_4;
-  animation-duration:2600ms;
-  animation-delay:-1800ms;
-  top:4.0em;
-  z-index:4;
-  opacity:0.5;
-  background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="480" height="2000" viewBox="0 0 12 2000"><path transform="rotate(-180 123,999.5)" fill-rule="evenodd" clip-rule="evenodd" fill="#153C65" d="M480 2000c-100.8 0-141.6-39.892-240-39.892S98.4 2000 0 2000V0h480v2000z"/></svg>');
-  background-position:top left;
+.banc__poisson--2 {
+  position: absolute;
+  bottom:50vh;
+  left: 10vw;
+  width: 20vw;
+  height: 20vh;
 }
 
-.wave_5
-{
-  animation-name:wave_5;
-  animation-duration:3000ms;
-  animation-delay:-3000ms;
-  top:5.0em;
-  z-index:5;
-  opacity:0.75;
-  background-image:url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="480" height="2000" viewBox="0 0 12 2000"><path transform="rotate(-180 123,999.5)" fill-rule="evenodd" clip-rule="evenodd" fill="#153C65" d="M480 2000c-100.8 0-141.6-39.892-240-39.892S98.4 2000 0 2000V0h480v2000z"/></svg>');
-  background-position:top right;
+.wave--1 {
+  top:15vh;
+  left:15vw;
+  width: 52px;
+  height: 43px;
+  background-image: url('../assets/img/vibration/sea-waves3.png');
 }
 
-/* Wave animations */
-
-@keyframes wave_1
-{
-	from { transform: rotate(0deg)   translatey(-0.61em) rotate(0deg)    ; }
-	to   { transform: rotate(360deg) translatey(-0.61em) rotate(-360deg) ; }
+.wave--2 {
+  bottom:15vh;
+  left:15vw;
+  width: 52px;
+  height: 43px;
+  background-image: url('../assets/img/vibration/sea-waves.png');
 }
 
-@keyframes wave_2
-{
-	from { transform: rotate(0deg)   translatey(-0.77em) rotate(0deg)    ; }
-	to   { transform: rotate(360deg) translatey(-0.77em) rotate(-360deg) ; }
-}
-
-@keyframes wave_3
-{
-	from { transform: rotate(0deg)   translatey(-0.96em) rotate(0deg)    ; }
-	to   { transform: rotate(360deg) translatey(-0.96em) rotate(-360deg) ; }
-}
-
-@keyframes wave_4
-{
-	from { transform: rotate(0deg)   translatey(-1.2em) rotate(0deg)    ; }
-	to   { transform: rotate(360deg) translatey(-1.2em) rotate(-360deg) ; }
-}
-
-@keyframes wave_5
-{
-	from { transform: rotate(0deg)   translatey(-1.5em) rotate(0deg)    ; }
-	to   { transform: rotate(360deg) translatey(-1.5em) rotate(-360deg) ; }
+.wave--3 {
+  bottom:55vh;
+  right:25vw;
+  width: 52px;
+  height: 43px;
+  background-image: url('../assets/img/vibration/sea-waves3.png');
 }
 </style>
