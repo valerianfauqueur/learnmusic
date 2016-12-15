6f158d25d5f32daa67531e28636f5b9f4a98ffd8
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-12-16 00:59:11 +0100

    [pages:home] Update page component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,34 +1,33 @@
 <template>
   <div id="home">
-    <router-link to="/vibration#air">Vibration</router-link>
-    <router-link to="/frequency">Frequency</router-link>
-    <button v-on:click="showModal = true">showModal</button>
-    <modal class='question' v-show="showModal"  @close="showModal = false">
-      <h3 class='question__text' slot="header">Que ressens-tu ?</h3>
-      <div class='question__answers' slot="body">
-        <answer v-bind:isTrue="false" answer="Des vibrations"></answer>
-        <answer v-bind:isTrue="true" answer="Du lait au chocolat"></answer>
-        <answer v-bind:isTrue="false" answer="Un tremblement de terre"></answer>
+    <section class='ctn-home'>
+      <div class="navbar">
+        <img src="../assets/img/logo.png">
       </div>
-      <router-link slot="footer" to="/frequency">Frequency</router-link>
-    </modal>
+      <div class="bg-castle">
+      </div>
+      <div class="ctn-info">
+        Bienvenue sur Orchestra,<br>
+        Ce soir vous dirigez un orchestre.
+      </div>
+      <div class='btn-spec'>
+        <buttonLink target="/orchestre" class="valign__content btn"></buttonLink>
+      </div>
+      <div class="ctn-inter">
+        <img src="../assets/img/icons/headphone.png">
+        Pour une meilleur expérience l'utilisation d'un casque <br /> est fortement recommandé
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Modal from '../components/Modal';
-import Answer from '../components/Answer';
+import ButtonLink from '../components/ButtonLink';
 
 export default {
   name: 'home',
-  data() {
-    return {
-      showModal: false,
-    };
-  },
   components: {
-    Modal,
-    Answer,
+    ButtonLink,
   },
 };
 </script>
@@ -39,13 +38,74 @@ export default {
 
 <style lang="scss" scoped>
 
-.question__answers {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+.ctn-home{
+  width:100%;
+  height:100%;
+  position: absolute;
+  background: linear-gradient(45deg, #332d2d 0%,#675757 100%);
+}
+.navbar{
+  display:flex;
+  justify-content:space-between;
+  padding:20px;
+}
+.navbar a{
+  position:relative;
+  margin-top:7px;
+  font-family:Montserrat;
+  font-size:17px;
+  color:#fff;
+  text-transform:capitalize;
+  text-decoration:none;
+  list-style-type:none;
+}
+.bg-castle{
+  background-image:url('../assets/img/palais-garnier.png');
+  width:100%;
+  height:100%;
+  margin-top:-80px;
+  z-index:0;
+  position:absolute;
+  background-repeat:no-repeat;
+  background-position:center center;
+  background-size:auto;
+}
+.ctn-info{
+  opacity:1;
+  color:#d8cfbc;
+  font-family:Didot;
+  font-size:1.9em;
+  text-align:center;
+  margin:auto;
+  padding:120px;
+}
+.ctn-inter{
+  display:flex;
+  align-items:center;
+  flex-direction:row-reverse;
+  justify-content:space-between;
+  flex-flow:column-reverse;
+  padding:20px;
+  justify-content:center;
+  color:#6a6060;
+  font-size:0.8em;
+  text-align:center;
+  font-family:Montserrat;
 }
 
-.question__text {
-  color:red;
+.valign {
+    font-size: 0
 }
+.valign:before {
+    content: "";
+    display: inline-block;
+    height: 100%;
+    width: 0;
+    vertical-align: middle
+}
+.valign .valign__content {
+    vertical-align: middle;
+    display: inline-block
+}
+
 </style>
